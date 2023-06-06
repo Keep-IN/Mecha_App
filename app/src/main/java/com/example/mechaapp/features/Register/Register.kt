ce89fd0258323644ca5fc6a69002fefcf9583a0e
Kevin <kevinraihanr45@gmail.com>
2023-06-07 00:17:44 +0700

    Added forgot password

--- app/src/main/java/com/example/mechaapp/features/Register/Register.kt
+++ app/src/main/java/com/example/mechaapp/features/Register/Register.kt
@@ -1,7 +1,8 @@
-package com.example.mechaapp
+package com.example.mechaapp.features.Register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mechaapp.R
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
