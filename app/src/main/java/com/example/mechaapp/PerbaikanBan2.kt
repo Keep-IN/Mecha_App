0b9ff9d49f2b232c600267a925cc6afa5a91954e
LaodeGhazy <ghazylaode002@gmail.com>
2023-06-08 15:43:31 +0700

    INI PIN LAgi

---
+++ app/src/main/java/com/example/mechaapp/PerbaikanBan2.kt
@@ -0,0 +1,11 @@
+package com.example.mechaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PerbaikanBan2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perbaikan_ban2)
+    }
+}
