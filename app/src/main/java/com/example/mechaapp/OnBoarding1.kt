34e9d6f7bca956d2586c199c1ac4c884b4b42743
LaodeGhazy <ghazylaode002@gmail.com>
2023-06-08 14:54:00 +0700

    INI PIN

---
+++ app/src/main/java/com/example/mechaapp/OnBoarding1.kt
@@ -0,0 +1,11 @@
+package com.example.mechaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoarding1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding1)
+    }
+}
