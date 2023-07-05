e75e09406dbdaf679c398a14e3c46a76c7d29663
LaodeGhazy <ghazylaode002@gmail.com>
2023-07-05 14:19:19 +0700

    Update HomePage

---
+++ app/src/main/java/com/example/mechaapp/features/data/adapter/SpotlightListAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.mechaapp.features.data.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.mechaapp.databinding.SpotlightListViewBinding
+import com.example.mechaapp.features.data.model.SpotlightItemModel
+
+class SpotlightListAdapter : RecyclerView.Adapter<SpotlightListAdapter.ViewHolder>()
+{
+    private var itemListener: ((SpotlightItemModel) -> Unit)? = null
+    private val data: MutableList<SpotlightItemModel> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            SpotlightListViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.setData(data[position], itemListener)
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    fun submitList(list: List<SpotlightItemModel>) {
+        val initSize = itemCount
+        data.clear()
+        notifyItemRangeRemoved(0, initSize)
+        data.addAll(list)
+        notifyItemRangeInserted(0, data.size)
+    }
+
+    inner class ViewHolder(private val binding: SpotlightListViewBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun setData(item: SpotlightItemModel, Listener: ((SpotlightItemModel) -> Unit)?) {
+
+            binding.root.setOnClickListener {
+                Listener?.invoke(item)
+            }
+            Glide
+                .with(binding.root.context)
+                .load(item.img)
+                .into(binding.impiwSpot)
+        }
+
+    }
+}
