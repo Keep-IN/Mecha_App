11be1f202cce609a0368d7629d6dc64a647fdfdd
LaodeGhazy <ghazylaode002@gmail.com>
2023-09-12 01:53:55 +0700

    bebas aja

--- app/src/main/java/com/example/mechaapp/features/DetailPesanan/DetailPesanan.kt
+++ app/src/main/java/com/example/mechaapp/features/DetailPesanan/DetailPesanan.kt
@@ -3,7 +3,9 @@ package com.example.mechaapp.features.DetailPesanan
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mechaapp.R
+import com.example.mechaapp.data.adapter.DetailMontirAdapter
 import com.example.mechaapp.databinding.ActivityDetailPesananBinding
 import com.example.mechaapp.databinding.ActivityMainBinding
 import com.example.mechaapp.features.History.HistoryFragment
@@ -11,10 +13,20 @@ import com.example.mechaapp.features.Login.ForgetPassword
 
 class DetailPesanan : AppCompatActivity() {
     private lateinit var binding: ActivityDetailPesananBinding
+    private val adapterDetailuser: DetailMontirAdapter by lazy { DetailMontirAdapter () }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityDetailPesananBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        val view = binding.root
+        setContentView(view)
+
+
+        val layoutManager = LinearLayoutManager(this)
+        //adapterDetailmontir.submitList(DataPembayaran.metodeList)
+        binding.rvDetailuser.adapter = adapterDetailuser
+        binding.rvDetailuser.layoutManager = layoutManager
+
         binding.cvMetode.setOnClickListener {
             startActivity(Intent(this, MetodePembayaran::class.java ))
         }
